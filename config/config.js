0ebeddb88209f3cc56d19de6af08bf9e6bdc2054
sunanda <sunandach88@gmail.com>
2013-12-13 15:29:14 +0530

    cluster feature is added(master and workers)

--- config/config.js
+++ config/config.js
@@ -3,44 +3,36 @@
  */
 
 module.exports = {
-	environment : 'development',
-	development : {
-		dbPath : 'C://tingoDB//data',
-		app : {
-			name : 'proxy-etc',
-			hostname : 'localhost'
-		},
-		http : {
-			port : 80,
-		},
-/*		https : {
-			port : 443,
-			keyPath : './config/keys/4946453_localhost.key',
-			certPath : './config/keys/4946453_localhost.cert'
-		},*/
-		credentials : {
-			username : "admin",
-			password : "admin"
-		}
+	dbPath : 'C://tingoDB//data',
+	maxSockets : 100,
+	workers : 4,
+	tcpTimeout : 90,
+	httpKeepAlive : true,
+	//UI
+	app : {
+		name : 'proxy-etc',
+		hostname : 'localhost',
+		port : 3000,
 	},
-	test : {
-		dbPath : 'C://tingoDB//data',
-		app : {
-			name : 'proxy-etc',
-			hostname : 'localhost'
-		},
-		http : {
-			port : 80,
-		},
-/*		https : {
-			port : 443,
-			keyPath : './config/keys/4946453_localhost.key',
-			certPath : './config/keys/4946453_localhost.cert'
-		},*/
-		credentials : {
-			username : "admin",
-			password : "admin"
-		}
+	
+	//proxy
+	http : {
+		hostname : 'localhost',
+		port : 80,
 	},
-	production : {}
+	https : {
+		hostname : 'localhost',
+		port : 443,
+		keyPath : './config/keys/4946453_localhost.key',
+		certPath : './config/keys/4946453_localhost.cert'
+	},
+	memoryMonitor : {
+		memoryLimit : 100, //MBs
+		gracefulWait : 30, //seconds
+		checkInterval : 60 //seconds
+	},
+	credentials : {
+		username : "admin",
+		password : "admin"
+	}
 };
