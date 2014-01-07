7949f040b9e2ee931436d6b95d872ba56686f195
Thanneer <tm@beyondbytes.co.in>
2014-01-08 02:55:14 +0530

    commented https block

--- config/config.js
+++ config/config.js
@@ -20,12 +20,12 @@ module.exports = {
 		hostname : 'localhost',
 		port : 80,
 	},
-	https : {
-		hostname : 'localhost',
-		port : 443,
-		keyPath : '/opt/bbytes/proxy-etc/config/keys/errzero.private.pem',
-		certPath : '/opt/bbytes/proxy-etc/config/keys/errzero.public.pem'
-	},
+	// https : {
+	//	hostname : 'localhost',
+	//	port : 443,
+	//	keyPath : '/opt/bbytes/proxy-etc/config/keys/errzero.private.pem',
+	//	certPath : '/opt/bbytes/proxy-etc/config/keys/errzero.public.pem'
+	// },
 	memoryMonitor : {
 		memoryLimit : 100, //MBs
 		gracefulWait : 30, //seconds
